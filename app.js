cba66be119a561608a549756c1ac4e2e67b4c9c1
Pablo Damián Gómez <pgomez@despegar.com>
2018-07-25 18:52:44 -0300

    gitignore and apps

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const urls = require('./url-array.js');
+const fs = require('fs');
+const path = './urllist-redirects.txt';
 
-urls.map(url => {
-    console.log(url);
+fs.open(path, 'w', function(err, fd) {  
+    if (err) throw 'could not open file: ' + err;
+
+    let urlsToWrite = urls.map(url => { return url + '\n' });
+    
+    fs.write(fd, urlsToWrite, 0, urlsToWrite.length, function(err) {
+        if (err) throw 'error writing file: ' + err;
+        fs.close(fd, function() {
+            console.log('wrote the file successfully');
+        });
+    });
 });
